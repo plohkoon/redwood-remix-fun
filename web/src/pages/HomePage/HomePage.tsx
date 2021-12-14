5675a74819bce31905e7a5ceb660dc5c8a32f698
Gregory Huber <greg@ruboss.com>
2021-12-13 19:41:31 -0800

    homepage

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -1,25 +1,7 @@
-import { Link, routes } from '@redwoodjs/router'
-import { MetaTags } from '@redwoodjs/web'
+import BlogPostsCell from 'src/components/BlogPostsCell'
 
 const HomePage = () => {
-  return (
-    <>
-      <MetaTags
-        title="Home"
-        // description="Home description"
-        /* you should un-comment description and add a unique description, 155 characters or less
-        You can look at this documentation for best practices : https://developers.google.com/search/docs/advanced/appearance/good-titles-snippets */
-      />
-      <h1>HomePage</h1>
-      <p>
-        Find me in <code>./web/src/pages/HomePage/HomePage.tsx</code>
-      </p>
-      <p>
-        My default route is named <code>home</code>, link to me with `
-        <Link to={routes.home()}>Home</Link>`
-      </p>
-    </>
-  )
+  return <BlogPostsCell />
 }
 
 export default HomePage
